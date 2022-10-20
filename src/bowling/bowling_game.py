ac0f632a71cab2622ce132743f7227035ef8a745
Adam Gartee <agartee@gmail.com>
2022-10-20 18:02:43 -0400

    added concept of two-throw frames for tracking throws

--- src/bowling/bowling_game.py
+++ src/bowling/bowling_game.py
@@ -1,13 +1,20 @@
+from bowling.bowling_frame import BowlingFrame
+
+
 class BowlingGame:
     def __init__(self):
-        self._throws = []
+        self._frames = []
+        for i in range(0, 10):
+            self._frames.append(BowlingFrame(idx=i))
+
+        self._current_frame = self.frames[0]
 
     @property
-    def throws(self):
-        return self._throws.copy()
+    def frames(self):
+        return self._frames.copy()
 
     def record_throw(self, pins):
-        if pins not in range(0, 10):
-            raise ValueError(f"Invalid number of pins: {pins}")
+        if self._current_frame.is_complete:
+            self._current_frame = self.frames[self._current_frame.idx + 1]
 
-        self._throws.append(pins)
+        self._current_frame.record_throw(pins)
--- src/tests/bowling/test_bowling_game.py
+++ src/tests/bowling/test_bowling_game.py
@@ -1,29 +1,29 @@
-import pytest
 from bowling.bowling_game import BowlingGame
+from bowling.bowling_frame import BowlingFrame
 
 
 def test_record_throw_stores_pins():
     game = BowlingGame()
     game.record_throw(5)
 
-    assert len(game.throws) == 1
-    assert game.throws[0] == 5
-
-
-@pytest.mark.parametrize("pins", [-1, 11, 5.5])
-def test_record_throw_raises_exception_with_invalid_pins(pins):
-    game = BowlingGame()
-
-    with pytest.raises(ValueError) as err:
-        game.record_throw(pins)
-
-    assert "Invalid number of pins" in str(err.value)
+    frame = game.frames[0]
+    assert frame == BowlingFrame(first_throw=5)
 
 
 def test_game_throws_cannot_be_mutated_outside_game():
     game = BowlingGame()
-    game.record_throw(5)
-    game.throws.append(1)
+    initial_frame_count = len(game.frames)
 
-    assert len(game.throws) == 1
-    assert game.throws[0] == 5
+    game.frames.append(BowlingFrame())
+
+    assert len(game.frames) == initial_frame_count
+
+
+def test_game_records_throw_on_next_frame_when_frame_is_complete():
+    game = BowlingGame()
+    game.record_throw(1)
+    game.record_throw(2)
+    game.record_throw(3)
+
+    second_frame = game.frames[1]
+    assert second_frame.first_throw == 3
