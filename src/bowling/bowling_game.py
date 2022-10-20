54e47888a9784ce3e12cc1faeda53e8345430d58
Adam Gartee <agartee@gmail.com>
2022-10-20 16:15:10 -0400

    Game defined as well as basic throw recording

--- src/bowling/bowling_game.py
+++ src/bowling/bowling_game.py
@@ -1,2 +1,13 @@
 class BowlingGame:
-    pass
+    def __init__(self):
+        self._throws = []
+
+    @property
+    def throws(self):
+        return self._throws.copy()
+
+    def record_throw(self, pins):
+        if pins not in range(0, 10):
+            raise ValueError(f"Invalid number of pins: {pins}")
+
+        self._throws.append(pins)
