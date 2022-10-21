584b78ddc276fa025c9d1de36ffcdbfbce63a5ec
Adam Gartee <agartee@gmail.com>
2022-10-21 07:18:08 -0400

    added frame and game scoring for basic throws (no bonuses)

--- src/bowling/bowling_game.py
+++ src/bowling/bowling_game.py
@@ -13,6 +13,23 @@ class BowlingGame:
     def frames(self):
         return self._frames.copy()
 
+    @property
+    def frame_scores(self):
+        scores = []
+        for frame in self.frames:
+            if frame.first_throw is None:
+                scores.append(None)
+                continue
+
+            scores.append(frame.first_throw + frame.second_throw)
+
+        return scores
+
+    @property
+    def total_score(self):
+        frame_scores = [0 if score is None else score for score in self.frame_scores]
+        return sum(frame_scores)
+
     def record_throw(self, pins):
         if self._current_frame.is_complete:
             self._current_frame = self.frames[self._current_frame.idx + 1]
