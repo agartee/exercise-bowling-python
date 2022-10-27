794bce6723f7eda9e7502b666f33788864aa4354
Adam Gartee <agartee@gmail.com>
2022-10-27 05:00:15 -0400

    cleanup support scripts

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,13 +1,38 @@
 $rootDir = (get-item $PSScriptRoot).Parent.FullName
 
 # **************************************************************************************
+# Deactivate Python Virtual Environment
+# **************************************************************************************
+deactivate | Out-Null
+
+# **************************************************************************************
 # Delete Python Virtual Environment (if exists)
 # **************************************************************************************
 $venvPath = "$($rootDir)\.venv"
 
 if (Test-Path -Path $venvPath) {
-    Remove-Item -Recurse -Force "$($rootDir)\.venv"
+    Remove-Item -Recurse -Force "$($venvPath)"
     Write-Host "Python virtual environment deleted." -ForegroundColor Green
+}
+
+# **************************************************************************************
+# Delete pytest Cache (if exists)
+# **************************************************************************************
+$pytestCachePath = "$($rootDir)\.pytest_cache"
+
+if (Test-Path -Path $pytestCachePath) {
+    Remove-Item -Recurse -Force "$($pytestCachePath)"
+    Write-Host "pytest cache deleted." -ForegroundColor Green
+}
+
+# **************************************************************************************
+# Delete pytest Coverage File (if exists)
+# **************************************************************************************
+$pytestCoverageFilePath = "$($rootDir)\.coverage"
+
+if (Test-Path -Path $pytestCoverageFilePath) {
+    Remove-Item "$($pytestCoverageFilePath)"
+    Write-Host "pytest coverage file deleted." -ForegroundColor Green
 }
 
 # **************************************************************************************
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,6 +16,11 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ];
 fi
 
 # **************************************************************************************
+# Deactivate Python Virtual Environment
+# **************************************************************************************
+deactivate 2> /dev/null
+
+# **************************************************************************************
 # Delete Python Virtual Environment (if exists)
 # **************************************************************************************
 VENV_PATH="$ROOT_DIR/.venv"
@@ -23,6 +28,26 @@ VENV_PATH="$ROOT_DIR/.venv"
 if [ -d "$VENV_PATH" ]; 
     then rm -Rf $VENV_PATH; 
     echo -e "${GREEN}Python virtual environment deleted.${NC}"
+fi
+
+# **************************************************************************************
+# Delete pytest Cache (if exists)
+# **************************************************************************************
+PYTEST_CACHE_PATH="$ROOT_DIR/.pytest_cache"
+
+if [ -d "$PYTEST_CACHE_PATH" ]; 
+    then rm -Rf $PYTEST_CACHE_PATH; 
+    echo -e "${GREEN}pytest cache deleted.${NC}"
+fi
+
+# **************************************************************************************
+# Delete pytest Coverage File (if exists)
+# **************************************************************************************
+PYTEST_COVERAGE_FILE_PATH="$ROOT_DIR/.coverage"
+
+if [ -d "$PYTEST_COVERAGE_FILE_PATH" ]; 
+    then rm $PYTEST_COVERAGE_FILE_PATH; 
+    echo -e "${GREEN}pytest coverage file deleted.${NC}"
 fi
 
 # **************************************************************************************
